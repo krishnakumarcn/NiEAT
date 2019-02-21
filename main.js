396d8f6d8112b258f57388e45dab95fbb84ec72f
Krishnakumar CN <krishnacnskp@gmail.com>
2019-02-21 18:29:44 +0530

    helperintent return

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ var config = {
   messagingSenderId: "793060807751"
 };
 
-
 firebase.initializeApp(config);
 
 const restService = express();
@@ -46,6 +45,59 @@ restService.post("/orderMeal", function(req, res) {
   //   });
 
   //
+  var helperIntent = {
+    payload: {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
+            {
+              simpleResponse: {
+                textToSpeech: "Choose a item"
+              }
+            }
+          ]
+        },
+        systemIntent: {
+          intent: "actions.intent.OPTION",
+          data: {
+            "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+            listSelect: {
+              title: "Hello",
+              items: [
+                {
+                  optionInfo: {
+                    key: "first title key"
+                  },
+                  description: "first description",
+                  image: {
+                    url:
+                      "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                    accessibilityText: "first alt"
+                  },
+                  title: "first title"
+                },
+                {
+                  optionInfo: {
+                    key: "second"
+                  },
+                  description: "second description",
+                  image: {
+                    url:
+                      "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                    accessibilityText: "second alt"
+                  },
+                  title: "second title"
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  };
+
+  return res.json(helperIntent);
 
   return res.json({
     //     speech: state + " is the speech",
