49d2b3df41d062387d70466b4aa5ab4364401aa2
Krishnakumar CN <krishnacnskp@gmail.com>
2019-02-22 16:57:49 +0530

    date time edited

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ restService.post("/orderMeal", async function(req, res) {
       if (req.body.queryResult.parameters["number-integer"])
         quantity = req.body.queryResult.parameters["number-integer"];
 
-      datetime = new Date().valueOf().toString();
+      datetime = getDateTime();  //new Data().toString()
 
       await writeToDB(
         datetime,
@@ -209,6 +209,31 @@ async function writeToDB(
       };
       res.json(x);
     });
+}
+
+function getDateTime() {
+
+  var date = new Date();
+
+  var hour = date.getHours();
+  hour = (hour < 10 ? "0" : "") + hour;
+
+  var min  = date.getMinutes();
+  min = (min < 10 ? "0" : "") + min;
+
+  var sec  = date.getSeconds();
+  sec = (sec < 10 ? "0" : "") + sec;
+
+  var year = date.getFullYear();
+
+  var month = date.getMonth() + 1;
+  month = (month < 10 ? "0" : "") + month;
+
+  var day  = date.getDate();
+  day = (day < 10 ? "0" : "") + day;
+
+  return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
 }
 
 // restService.post("/audio", function(req, res) {
